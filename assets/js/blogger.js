481e667bf6a57116cc3edb02a08b9a8423723696
Luda <luisdavidgd@gmail.com>
2019-03-15 18:03:18 -0500

    Update blog section view

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -3,24 +3,30 @@ var posts = [];
 $.get(
   ludablog_json_in_script,
   function (data) {
+    console.log(data.feed.entry);
     $.each(data.feed.entry, function (i, item) {
 
-      var name = item.author[0].name.$t;
-      var category = item.category[0].term;
+      var author = item.author[0].name.$t;
       var title = item.title.$t;
       var updated = item.updated.$t;
       var link = item.link[4].href;
-      var content = item.content.$t;
-      //var image = item.media$thumbnail.url;
+      var summary = item.summary.$t;
+      var comments = item.thr$total.$t
+      var thumbnail = item.media$thumbnail.url;
+      // remove thumbnail property with replace function xD
+      var image = thumbnail.replace('s72-c/', '');
+      // convert weird format date into human readable w/ formatDate function (own) D:
+      var date = formatDate(new Date(updated));
 
       post = {};
-      post['name'] = name;
-      post['category'] = category;
+
+      post['author'] = author;
       post['title'] = title;
-      post['updated'] = updated;
+      post['date'] = date;
       post['link'] = link;
-      //post['image'] = image;
-      post['content'] = content;
+      post['image'] = image;
+      post['comments'] = comments;
+      post['summary'] = summary;
 
       posts.push(post);
     });
@@ -28,9 +34,28 @@ $.get(
   "jsonp")
   .done(function () {
     $.each(posts, function (i, item) {
-      $('#post-' + i + ' .name').html(item.name);
-      $('#post-' + i + ' .title').html(item.title);
-      $('#post-' + i + ' .link').attr("href", item.link);
-      //$('#post-' + i + ' .content').html(item.content);
+      $('#post-' + i + ' .post-author').html(item.author);
+      $('#post-' + i + ' .post-title').html(item.title);
+      $('#post-' + i + ' .post-link').attr("href", item.link);
+      $('#post-' + i + ' .post-img').attr("src", item.image);
+      $('#post-' + i + ' .post-summary').html(item.summary);
+      $('#post-' + i + ' .post-comments').html(item.comments);
+      $('#post-' + i + ' .post-date').html(item.date);
     });
   });
+
+function formatDate(date) {
+  var monthNames = [
+    "Enero", "Febrero", "Marzo",
+    "Abril", "Mayo", "Junio", "Julio",
+    "Agosto", "Setiembre", "Octubre",
+    "Noviembre", "Diciembre"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  //return day + ' de ' + monthNames[monthIndex] + ' de ' + year;
+  return monthNames[monthIndex] + ' ' + day + ', ' + year;
+}
