8871be1f8c97fad4423538f221e354ae6ebaffcc
Luis David <luisdavidgd@gmail.com>
2019-06-07 13:40:57 -0500

    Contact form

--- demos/nsgmilan/assets/php/contact.php
+++ demos/nsgmilan/assets/php/contact.php
@@ -1,2 +1,18 @@
 <?php
-print_r($_POST);
+if($_POST) {
+  $to = "ventas@nsgmilan.com";
+  $name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+  $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+  $phone = filter_var($_POST["phone"], FILTER_SANITIZE_STRING);
+  $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
+  $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+  $body = "Name: $name\nE-mail: $email\nPhone: $phone\nMessage: $message";
+  
+  if(mail($to, $subject, $body)) {
+    $output = json_encode(array('success' => true));
+    die($output);
+  } else {
+    $output = json_encode(array('success' => false));
+    die($output);
+  }
+}
