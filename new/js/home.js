e66ac89e802ab3892789c19e2940ea858293afad
Luis Gallo <lgallo@ctdevelopers.com>
2021-12-12 19:15:40 -0500

    Layout updated

---
+++ new/js/home.js
@@ -0,0 +1,13 @@
+function home() {
+  return {
+    text: "",
+    textArray: ["Luda", "software developer", "your god."],
+    textIndex: 0,
+    charIndex: 0,
+    pauseEnd: 1500,
+    cursorSpeed: 420,
+    pauseStart: 20,
+    typeSpeed: 110,
+    direction: "forward",
+  };
+}
