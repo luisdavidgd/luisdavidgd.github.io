44b14bac4dcc4fb30313804005c70927b8b40bbb
Luis Gallo <lgallo@ctdevelopers.com>
2025-04-18 18:24:47 -0500

    Change template

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Navbar />
+    <HeroSection />
+    <AboutMe />
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import Navbar from './components/Navbar.vue'
+import HeroSection from './components/HeroSection.vue'
+import AboutMe from './components/AboutMe.vue'
+import Footer from './components/Footer.vue'
+</script>
+
