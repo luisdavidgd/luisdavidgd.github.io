66c4fa8ae6893206b6b0dba41a20664060880043
Luis Gallo <luisdavidgd@gmail.com>
2025-06-03 16:08:15 -0500

    feat: Update components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import HeroSection from "../lib/components/HeroSection.svelte";
+    import AboutMe from "../lib/components/AboutMe.svelte";
+    import Footer from "../lib/components/Footer.svelte";
+</script>
+
+<HeroSection />
+<AboutMe />
+<Footer />
