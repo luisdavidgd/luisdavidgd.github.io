bee2752e3f537af91a37c4274df8ce6ced69106f
Luis David <luisdavidgd@gmail.com>
2025-09-02 22:17:38 -0500

    added

--- astro/src/content/schema/blog.ts
+++ astro/src/content/schema/blog.ts
@@ -3,16 +3,18 @@ import { z, defineCollection } from "astro:content";
 
 export const blog = defineCollection({
   loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/posts" }),
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    description: z.string(),
-    author: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string()
-    }),
-    categories: z.array(z.string()),
-    tags: z.array(z.string())
-  })
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      description: z.string(),
+      author: z.string().optional(),
+      image: z.object({
+        url: z.string(),
+        alt: z.string()
+      }),
+      categories: z.array(z.string()),
+      tags: z.array(z.string()),
+      heroImage: image().optional()
+    })
 });
